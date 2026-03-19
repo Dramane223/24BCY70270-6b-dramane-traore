35546d727437e593c1955725fdf1d1ff6a6163e7
Dramane223 <abadramanetraore85@gmail.com>
2026-03-19 05:53:53 +0530

    fix vercel app.listen issue

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
